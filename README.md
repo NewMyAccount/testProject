389bf33cd02a763fcf1b6f2469d0908f54f1e94a
zhangxin <mainzhangxin@163.com>
2021-07-27 15:09:39 +0800

    add check cookies

--- README.md
+++ README.md
@@ -36,3 +36,17 @@
 
 
 ##tomcat、 spring、 spring mvc、 servlet、jvm 关系和联系 
+
+##脚本
+
+```sql
+    create table USER
+(
+    ID           INT auto_increment primary key NOT NULL ,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        VARCHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+);
+```
--- src/main/java/com/example/testproject/controller/IndexController.java
+++ src/main/java/com/example/testproject/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.example.testproject.controller;
 
+import com.example.testproject.mapper.UserMapper;
+import com.example.testproject.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: 张昕
@@ -13,14 +19,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/")
-    public String hello(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                //用传到服务器端的reqeust.cookie获取token，然后校验session是否存在
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
+    public String hello(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
     }
