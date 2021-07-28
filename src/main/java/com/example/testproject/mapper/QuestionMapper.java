cc61797368fb92c7cb0a1b34fb8a94b835d6c896
zhangxin <mainzhangxin@163.com>
2021-07-28 19:32:34 +0800

    finish html function of publish

---
+++ src/main/java/com/example/testproject/mapper/QuestionMapper.java
@@ -0,0 +1,17 @@
+package com.example.testproject.mapper;
+
+import com.example.testproject.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: 张昕
+ * @Date： 2021/7/28
+ * @Time: 16:18
+ **/
+@Mapper
+public interface QuestionMapper {
+    @Insert("Insert into question(title, description, gmt_create, gmt_modified, creator, tag) " +
+            "values(#{title}, #{description}, #{gmtCreate}, #{gmtModified}, #{creator}, #{tag})")
+    void create(Question question);
+}
