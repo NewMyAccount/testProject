bea40060e167fbefe9796cd09be239bac4c8e373
zhangxin <mainzhangxin@163.com>
2021-08-18 11:32:32 +0800

    finish edit and update profile problem

--- src/main/java/com/example/testproject/controller/PublishController.java
+++ src/main/java/com/example/testproject/controller/PublishController.java
@@ -1,12 +1,15 @@
 package com.example.testproject.controller;
 
+import com.example.testproject.dto.QuestionDTO;
 import com.example.testproject.mapper.QuestionMapper;
 import com.example.testproject.model.Question;
 import com.example.testproject.model.User;
+import com.example.testproject.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,7 +23,16 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PublishController {
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
+
+    @GetMapping("publish/{id}")
+    public String editQuestion(@PathVariable(name = "id") Integer id, Model model){
+        QuestionDTO question = questionService.findById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish() {
@@ -31,6 +43,7 @@ public class PublishController {
     public String doPublish(@RequestParam("title") String title,
                             @RequestParam("description") String description,
                             @RequestParam("tag") String tag,
+                            @RequestParam(name = "id", required = false) Integer id,
                             HttpServletRequest request,
                             Model model) {
         model.addAttribute("title", title);
@@ -53,15 +66,13 @@ public class PublishController {
             model.addAttribute("error", "用户未登录");
             return "publish";
         }
-
         Question question = new Question();
         question.setTitle(title);
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
     }
 }
